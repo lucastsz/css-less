8535382aff9ee1dbe7f5212ec9c3be56bb85fdf3
Lucas Souza <lucassouza.1000@hotmail.com>
2022-07-21 19:14:39 -0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('../views/less')
+})
+
+app.listen(3001, function () {
+  console.log('executando na porta 3001')
+})
